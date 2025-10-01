22f2ff20d4271d803f05d89f34139c22730598ca
brodeymc <brodeymc@icloud.com>
2025-10-01 14:29:26 -0400

    Started Simulation Controller
    Started working on simulation controller script

--- Assets/Scripts/SimulationController.cs
+++ Assets/Scripts/SimulationController.cs
@@ -1,6 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SimulationController : MonoBehaviour
 {
-    
+    private EnvironmentCell[,] grid;
+    private List<AnimalAgent> animals;
+    private Drought drought;
+
+    public float moisture;
+    public float density;
+
+    public SimulationController(int width, int height, int agents)
+    {
+        grid = new EnvironmentCell[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                grid[x, y] = new EnvironmentCell(moisture, density);
+            }
+        }
+    }
+
 }
